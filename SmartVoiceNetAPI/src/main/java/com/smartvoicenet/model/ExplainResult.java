d9c93af8ccfbe44697251e83509930a688cbb867
Lenovo <Lenovo@LAPTOP-CUDUT1QP>
2020-08-16 16:28:00 +0530

    Draft Version

---
+++ SmartVoiceNetAPI/src/main/java/com/smartvoicenet/model/ExplainResult.java
@@ -0,0 +1,64 @@
+package com.smartvoicenet.model;
+
+public class ExplainResult {
+	
+	private String inspectionResult;
+	private String authScore;
+	private String repeatationIndicator;
+	private String grammaticalError;
+	private String sensitiveHotspot;
+	private String urgencyIndicator;
+	private String smartProbeError;
+	private String smartProbeErrorQs;
+	
+	public String getInspectionResult() {
+		return inspectionResult;
+	}
+	public void setInspectionResult(String inspectionResult) {
+		this.inspectionResult = inspectionResult;
+	}
+	public String getAuthScore() {
+		return authScore;
+	}
+	public void setAuthScore(String authScore) {
+		this.authScore = authScore;
+	}
+	public String getRepeatationIndicator() {
+		return repeatationIndicator;
+	}
+	public void setRepeatationIndicator(String repeatationIndicator) {
+		this.repeatationIndicator = repeatationIndicator;
+	}
+	public String getGrammaticalError() {
+		return grammaticalError;
+	}
+	public void setGrammaticalError(String grammaticalError) {
+		this.grammaticalError = grammaticalError;
+	}
+	public String getSensitiveHotspot() {
+		return sensitiveHotspot;
+	}
+	public void setSensitiveHotspot(String sensitiveHotspot) {
+		this.sensitiveHotspot = sensitiveHotspot;
+	}
+	public String getUrgencyIndicator() {
+		return urgencyIndicator;
+	}
+	public void setUrgencyIndicator(String urgencyIndicator) {
+		this.urgencyIndicator = urgencyIndicator;
+	}
+	public String getSmartProbeError() {
+		return smartProbeError;
+	}
+	public void setSmartProbeError(String smartProbeError) {
+		this.smartProbeError = smartProbeError;
+	}
+	public String getSmartProbeErrorQs() {
+		return smartProbeErrorQs;
+	}
+	public void setSmartProbeErrorQs(String smartProbeErrorQs) {
+		this.smartProbeErrorQs = smartProbeErrorQs;
+	}
+	
+
+}
---
+++ SmartVoiceNetAPI/src/main/java/com/smartvoicenet/repository/ExplainResultRepo.java
@@ -0,0 +1,9 @@
+package com.smartvoicenet.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.smartvoicenet.model.ExplainResult;
+
+public interface ExplainResultRepo extends CrudRepository<ExplainResult, Long> {
+
+}
